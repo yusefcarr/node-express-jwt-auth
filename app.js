3950ef0cfcdcdc101c54bf603af565bc1d9464cb
yusef carr <ycarr99@gmail.com>
2024-10-08 08:35:38 -0400

    new push

--- app.js
+++ app.js
@@ -1,20 +1,21 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const authRoutes = require('./routes/authRoutes');
+const cookieParser = require('cookie-parser');
 
 const app = express();
 
-// middleware
 app.use(express.static('public'));
+app.use(express.json());
+app.use(cookieParser());
 
-// view engine
 app.set('view engine', 'ejs');
 
-// database connection
-const dbURI = 'mongodb+srv://yusef:test1234@cluster0.xo4er.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+const dbURI = 'mongodb+srv://shaun:test1234@cluster0.del96.mongodb.net/node-auth';
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true })
   .then((result) => app.listen(3000))
   .catch((err) => console.log(err));
 
-// routes
 app.get('/', (req, res) => res.render('home'));
 app.get('/smoothies', (req, res) => res.render('smoothies'));
+app.use(authRoutes);
--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,5 @@
 const User = require("../models/User");
+const jwt = require('jsonwebtoken');
 
 const handleErrors = (err) => {
   console.log(err.message, err.code);
@@ -10,14 +11,23 @@ const handleErrors = (err) => {
   }
 
   if (err.message.includes('user validation failed')) {
- 
+    // console.log(err);
     Object.values(err.errors).forEach(({ properties }) => {
+      // console.log(val);
+      // console.log(properties);
       errors[properties.path] = properties.message;
     });
   }
 
   return errors;
 }
+
+const maxAge = 3 * 24 * 60 * 60;
+const createToken = (id) => {
+  return jwt.sign({ id }, 'net ninja secret', {
+    expiresIn: maxAge
+  });
+};
 
 module.exports.signup_get = (req, res) => {
   res.render('signup');
@@ -32,7 +42,9 @@ module.exports.signup_post = async (req, res) => {
 
   try {
     const user = await User.create({ email, password });
-    res.status(201).json(user);
+    const token = createToken(user._id);
+    res.cookie('jwt', token, { httpOnly: true, maxAge: maxAge * 1000 });
+    res.status(201).json({ user: user._id });
   }
   catch(err) {
     const errors = handleErrors(err);
@@ -44,6 +56,11 @@ module.exports.signup_post = async (req, res) => {
 module.exports.login_post = async (req, res) => {
   const { email, password } = req.body;
 
-  console.log(email, password);
-  res.send('user login');
+  try {
+    const user = await User.login(email, password);
+    res.status(200).json({ user: user._id });
+  } catch (err) {
+    res.status(400).json({});
+  }
+
 }
